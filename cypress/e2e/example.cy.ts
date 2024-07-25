d0015ce0bd2614562fa3ffc38d29e4b66a709c87
Gülay YILMAZ <gulay.yilmaz@developia.io>
2024-07-25 10:33:31 +0300

    feat: add multi devices test

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,34 +1,51 @@
+const sizes: (Cypress.ViewportPreset | [number, number])[] = [
+  "iphone-6",
+  "ipad-2",
+  [1024, 768],
+];
+
 describe("Example Home Page Tests", () => {
-  it("should display the main elements", () => {
-    cy.visit("http://localhost:3000/");
-    // Check for main container
-    cy.get("main").should("exist");
+  sizes.forEach((size) => {
+    it(`should display the main elements on ${
+      Array.isArray(size) ? `${size[0]}x${size[1]}` : size
+    }`, () => {
+      cy.visit("http://localhost:3000/");
 
-    // Verify "Get started by editing" text
-    cy.contains("Get started by editing").should("be.visible");
+      if (Array.isArray(size)) {
+        cy.viewport(size[0], size[1]);
+      } else {
+        cy.viewport(size as Cypress.ViewportPreset);
+      }
 
-    // Ensure the Vercel logo image is present
-    cy.get('img[alt="Vercel Logo"]').should("be.visible");
+      // Check for main container
+      cy.get("main").should("exist");
 
-    // Confirm the Next.js logo is visible
-    cy.get('img[alt="Next.js Logo"]').should("be.visible");
+      // Verify "Get started by editing" text
+      cy.contains("Get started by editing").should("be.visible");
 
-    // Check for the presence and visibility of links
-    const links = [
-      { text: "Docs", href: /https:\/\/nextjs\.org\/docs/ },
-      { text: "Learn", href: /https:\/\/nextjs\.org\/learn/ },
-      {
-        text: "Templates",
-        href: /https:\/\/vercel\.com\/templates\?framework=next\.js/,
-      },
-      { text: "Deploy", href: /https:\/\/vercel\.com\/new/ },
-    ];
+      // Ensure the Vercel logo image is present
+      cy.get('img[alt="Vercel Logo"]').should("be.visible");
 
-    links.forEach((link) => {
-      cy.contains("a", link.text)
-        .should("be.visible")
-        .and("have.attr", "href")
-        .and("match", link.href);
+      // Confirm the Next.js logo is visible
+      cy.get('img[alt="Next.js Logo"]').should("be.visible");
+
+      // Check for the presence and visibility of links
+      const links = [
+        { text: "Docs", href: /https:\/\/nextjs\.org\/docs/ },
+        { text: "Learn", href: /https:\/\/nextjs\.org\/learn/ },
+        {
+          text: "Templates",
+          href: /https:\/\/vercel\.com\/templates\?framework=next\.js/,
+        },
+        { text: "Deploy", href: /https:\/\/vercel\.com\/new/ },
+      ];
+
+      links.forEach((link) => {
+        cy.contains("a", link.text)
+          .should("be.visible")
+          .and("have.attr", "href")
+          .and("match", link.href);
+      });
     });
   });
 });
